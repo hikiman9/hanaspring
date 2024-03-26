06c3bf70dee7ad51ffba372fbf0eb4214ea7e745
hikiman <skyainq@naver.com>
2024-03-26 14:18:32 +0900

    240326(Into Spring Boot)

--- sb1/src/main/java/com/hana/Sb1Application.java
+++ sb1/src/main/java/com/hana/Sb1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sb1Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
         SpringApplication.run(Sb1Application.class, args);
     }
 
