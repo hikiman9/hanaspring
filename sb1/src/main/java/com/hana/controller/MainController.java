26bee40b1106e1f9b77e786d34ac6096d8077311
hikiman <skyainq@naver.com>
2024-03-26 13:21:29 +0900

    240326(Into Spring Boot)

--- sb1/src/main/java/com/hana/controller/MainController.java
+++ sb1/src/main/java/com/hana/controller/MainController.java
@@ -1,15 +1,12 @@
 package com.hana.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
     @RequestMapping("/")
-    public String main(Model m){
-        m.addAttribute("data", "MyData");
+    public String main(){
         return "index";
     }
 }
---
+++ sb1/src/main/webapp/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 2024-03-26
+  Time: 오후 1:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Main Page</title>
+</head>
+<body>
+    <h1>Main Page</h1>
+</body>
+</html>
